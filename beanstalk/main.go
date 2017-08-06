da6ba92a930e07e9eae788e11663d7ae01d113bd
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2017-08-07 01:46:51 +0800

    Added the protocol description

--- beanstalk/main.go
+++ beanstalk/main.go
@@ -1,3 +1,74 @@
+// Tier 1:
+// - Handle Route:
+//   - USE "default"
+//   - PUT
+//     - body: token + locations
+//     - pri: timestamp
+//     - delay: 0 seconds
+//     - ttr: 3 seconds
+// - Handle Route Token:
+//   - USE "<token>"
+//   - RESERVE
+//   - Return result
+
+// Tier 2:
+// - Handle Task:
+//   - USE "default"
+//   - RESERVE
+//   - USE "<token>"
+//   - Loop
+//     - RESERVE
+//     - DELETE
+//       - id: id of job just reserved
+//   - Depending on the result:
+//     - Not Found:
+//       - PUT
+//         - body: "in progress" + trialCount (= 1)
+//         - pri: (current) timestamp
+//         - delay: 0 seconds
+//         - ttr: 0 seconds
+//     - trialCount != maxTrialCount
+//       - PUT
+//         - body: "in progress" + trialCount (+= 1)
+//         - pri: (current) timestamp
+//         - delay: 0 seconds
+//         - ttr: 0 seconds
+//     - trialCount == maxTrialCount
+//       - PUT
+//         - body: "failure" + trialCount (= maxTrialCount)
+//         - pri: (current) timestamp
+//         - delay: 0 seconds
+//         - ttr: 0 seconds
+//       - Return
+//   - Google Maps
+//     - Use goroutine to parallel the queries
+//     - Or see whether the API allows many questions in one trip
+//   - Travelling Salesman
+//   - PUT
+//     - body: "success" + path + other results
+//     - pri: (current) timestamp
+//     - delay: 0 seconds
+//     - ttr: 0 seconds
+//   - USE "garbage"
+//   - PUT
+//     - body: token
+//     - pri: (current) timestamp
+//     - delay: 600 seconds
+//     - ttr: 0 seconds
+//   - USE "default"
+//   - DELETE
+//     - id: id of the job
+
+// Tier C:
+// - Loop
+//   - USE "garbage"
+//   - RESERVE
+//   - USE "<token>"
+//     - Loop
+//       - RESERVE
+//       - DELETE
+//         - id: id of the job just reserved
+
 package main
 
 import (
