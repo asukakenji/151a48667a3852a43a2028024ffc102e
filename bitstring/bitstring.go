d9c23778d0dbd871afcacf6e2c4c5170015958fe
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2017-08-14 02:17:25 +0800

    Updated bitstring/: Used function style instaead of struct style API; Removed useless functions and added new functions

--- bitstring/bitstring.go
+++ bitstring/bitstring.go
@@ -1,51 +1,114 @@
 package bitstring
 
-type BitString64 uint64
-
-const (
-	Zero = BitString64(0)
-)
-
-func (bs BitString64) LeftShift(amount uint) BitString64 {
-	return bs << amount
-}
-
-func (bs BitString64) RightShift(amount uint) BitString64 {
-	return bs >> amount
-}
-
 // index starts with 0.
-func (bs BitString64) IsSet(index uint) bool {
-	return bs&(1<<index) != 0
+func IsBitSetAtIndex(x uint64, index uint) bool {
+	return x&(1<<index) != 0
 }
 
-func (bs BitString64) Set(index uint) BitString64 {
-	return bs | (1 << index)
+// Hacker's Delight: Section 2-1
+func SetBitAtIndex(x uint64, index uint) uint64 {
+	return x | (1 << index)
 }
 
-func (bs BitString64) Reset(index uint) BitString64 {
-	return bs &^ (1 << index)
+// Hacker's Delight: Section 2-1
+func ResetBitAtIndex(x uint64, index uint) uint64 {
+	return x &^ (1 << index)
 }
 
-func (bs BitString64) UnsetRightmostOne() BitString64 {
-	return bs & (bs - 1)
+// Hacker's Delight: Section 2-1
+func SetRightmostZero(x uint64) uint64 {
+	return x | (x + 1)
 }
 
-func (bs BitString64) SetRightmostZero() BitString64 {
-	return bs | (bs + 1)
+// Hacker's Delight: Section 2-1
+func UnsetRightmostOne(x uint64) uint64 {
+	return x & (x - 1)
 }
 
 // Hacker's Delight: Figure 2-1
-func (bs BitString64) NextValueWithSameBitCount() BitString64 {
-	smallest := bs & -bs
-	ripple := bs + smallest
-	ones := bs ^ ripple
+func NextValueWithSameBitCount(x uint64) uint64 {
+	smallest := x & -x
+	ripple := x + smallest
+	ones := x ^ ripple
 	ones = (ones >> 2) / smallest
 	return ripple | ones
 }
 
-func (bs BitString64) InsertZero(index uint) BitString64 {
-	maskR := (BitString64(1) << index) - 1
+// Hacker's Delight: Figure 5-14 (ntz4a)
+func NumberOfTrailingZeros(x uint64) uint {
+	if x == 0 {
+		return 64
+	}
+	n := uint(63)
+	var a uint
+
+	b := uint(uint32(x))
+	if b != 0 {
+		n = n - 32
+		a = b
+	} else {
+		a = uint(uint32(x >> 32))
+	}
+
+	b = a << 16
+	if b != 0 {
+		n = n - 16
+		a = b
+	}
+
+	b = a << 8
+	if b != 0 {
+		n = n - 8
+		a = b
+	}
+
+	b = a << 4
+	if b != 0 {
+		n = n - 4
+		a = b
+	}
+
+	b = a << 2
+	if b != 0 {
+		n = n - 2
+		a = b
+	}
+
+	return n - ((a << 1) >> 31)
+}
+
+var tableForNumberOfTrailingZerosForPowerOfTwo = []uint{
+	63, 0, 58, 1, 59, 47, 53, 2,
+	60, 39, 48, 27, 54, 33, 42, 3,
+	61, 51, 37, 40, 49, 18, 28, 20,
+	55, 30, 34, 11, 43, 14, 22, 4,
+	62, 57, 46, 52, 38, 26, 32, 41,
+	50, 36, 17, 19, 29, 10, 13, 21,
+	56, 45, 25, 31, 35, 16, 9, 12,
+	44, 24, 15, 8, 23, 7, 6, 5,
+}
+
+// De Bruijn Sequence
+// https://stackoverflow.com/a/32178577/142239
+func NumberOfTrailingZerosForPowerOfTwo(x uint64) uint {
+	return tableForNumberOfTrailingZerosForPowerOfTwo[uint((x*0x7EDD5E59A4E28C2)>>58)]
+}
+
+func InsertZero(x uint64, index uint) uint64 {
+	maskR := (uint64(1) << index) - 1
 	maskL := ^maskR
-	return ((bs & maskL) << 1) | (bs & maskR)
+	return ((x & maskL) << 1) | (x & maskR)
+}
+
+func ToString(x uint64) string {
+	s := ""
+	for x != 0 {
+		if x&1 == 0 {
+			s = "0" + s
+		} else {
+			s = "1" + s
+		}
+		x >>= 1
+	}
+	return s
 }
--- bitstring/bitstring_test.go
+++ bitstring/bitstring_test.go
@@ -2,63 +2,9 @@ package bitstring
 
 import "testing"
 
-func TestLeftShift(t *testing.T) {
+func TestIsBitSetAtIndex(t *testing.T) {
 	cases := []struct {
-		bs       BitString64
-		amount   uint
-		expected BitString64
-	}{
-		{0, 0, 0},
-		{0, 1, 0},
-		{0, 2, 0},
-		{1, 0, 1},
-		{1, 1, 2},
-		{1, 2, 4},
-		{2, 0, 2},
-		{2, 1, 4},
-		{2, 2, 8},
-		{3, 0, 3},
-		{3, 1, 6},
-		{3, 2, 12},
-	}
-	for _, c := range cases {
-		got := c.bs.LeftShift(c.amount)
-		if got != c.expected {
-			t.Errorf("BitString(0x%016x).LeftShift(%d) = 0x%016x, expected 0x%016x", c.bs, c.amount, got, c.expected)
-		}
-	}
-}
-
-func TestRightShift(t *testing.T) {
-	cases := []struct {
-		bs       BitString64
-		amount   uint
-		expected BitString64
-	}{
-		{0, 0, 0},
-		{0, 1, 0},
-		{0, 2, 0},
-		{1, 0, 1},
-		{1, 1, 0},
-		{1, 2, 0},
-		{2, 0, 2},
-		{2, 1, 1},
-		{2, 2, 0},
-		{3, 0, 3},
-		{3, 1, 1},
-		{3, 2, 0},
-	}
-	for _, c := range cases {
-		got := c.bs.RightShift(c.amount)
-		if got != c.expected {
-			t.Errorf("BitString(0x%016x).RightShift(%d) = 0x%016x, expected 0x%016x", c.bs, c.amount, got, c.expected)
-		}
-	}
-}
-
-func TestIsSet(t *testing.T) {
-	cases := []struct {
-		bs       BitString64
+		x        uint64
 		index    uint
 		expected bool
 	}{
@@ -76,18 +22,21 @@ func TestIsSet(t *testing.T) {
 		{3, 2, false},
 	}
 	for _, c := range cases {
-		got := c.bs.IsSet(c.index)
+		got := IsBitSetAtIndex(c.x, c.index)
 		if got != c.expected {
-			t.Errorf("BitString(0x%016x).IsSet(%d) = %t, expected %t", c.bs, c.index, got, c.expected)
+			t.Errorf(
+				"IsBitSetAtIndex(0x%016x, %d) = %t, expected %t",
+				c.x, c.index, got, c.expected,
+			)
 		}
 	}
 }
 
-func TestSet(t *testing.T) {
+func TestSetBitAtIndex(t *testing.T) {
 	cases := []struct {
-		bs       BitString64
+		x        uint64
 		index    uint
-		expected BitString64
+		expected uint64
 	}{
 		{0, 0, 1},
 		{0, 1, 2},
@@ -103,18 +52,21 @@ func TestSet(t *testing.T) {
 		{3, 2, 7},
 	}
 	for _, c := range cases {
-		got := c.bs.Set(c.index)
+		got := SetBitAtIndex(c.x, c.index)
 		if got != c.expected {
-			t.Errorf("BitString(0x%016x).Set(%d) = 0x%016x, expected 0x%016x", c.bs, c.index, got, c.expected)
+			t.Errorf(
+				"SetBitAtIndex(0x%016x, %d) = 0x%016x, expected 0x%016x",
+				c.x, c.index, got, c.expected,
+			)
 		}
 	}
 }
 
-func TestReset(t *testing.T) {
+func TestResetBitAtIndex(t *testing.T) {
 	cases := []struct {
-		bs       BitString64
+		x        uint64
 		index    uint
-		expected BitString64
+		expected uint64
 	}{
 		{0, 0, 0},
 		{0, 1, 0},
@@ -130,48 +82,20 @@ func TestReset(t *testing.T) {
 		{3, 2, 3},
 	}
 	for _, c := range cases {
-		got := c.bs.Reset(c.index)
+		got := ResetBitAtIndex(c.x, c.index)
 		if got != c.expected {
-			t.Errorf("BitString(0x%016x).Reset(%d) = 0x%016x, expected 0x%016x", c.bs, c.index, got, c.expected)
-		}
-	}
-}
-
-func TestUnsetRightmostOne(t *testing.T) {
-	cases := []struct {
-		bs       BitString64
-		expected BitString64
-	}{
-		{0, 0},
-		{1, 0},
-		{2, 0},
-		{3, 2},
-		{4, 0},
-		{5, 4},
-		{6, 4},
-		{7, 6},
-		{8, 0},
-		{9, 8},
-		{10, 8},
-		{11, 10},
-		{12, 8},
-		{13, 12},
-		{14, 12},
-		{15, 14},
-		{16, 0},
-	}
-	for _, c := range cases {
-		got := c.bs.UnsetRightmostOne()
-		if got != c.expected {
-			t.Errorf("BitString(0x%016x).UnsetRightmostOne() = 0x%016x, expected 0x%016x", c.bs, got, c.expected)
+			t.Errorf(
+				"ResetBitAtIndex(0x%016x, %d) = 0x%016x, expected 0x%016x",
+				c.x, c.index, got, c.expected,
+			)
 		}
 	}
 }
 
 func TestSetRightmostZero(t *testing.T) {
 	cases := []struct {
-		bs       BitString64
-		expected BitString64
+		x        uint64
+		expected uint64
 	}{
 		{0, 1},
 		{1, 3},
@@ -192,17 +116,54 @@ func TestSetRightmostZero(t *testing.T) {
 		{16, 17},
 	}
 	for _, c := range cases {
-		got := c.bs.SetRightmostZero()
+		got := SetRightmostZero(c.x)
 		if got != c.expected {
-			t.Errorf("BitString(0x%016x).SetRightmostZero() = 0x%016x, expected 0x%016x", c.bs, got, c.expected)
+			t.Errorf(
+				"SetRightmostZero(0x%016x) = 0x%016x, expected 0x%016x",
+				c.x, got, c.expected,
+			)
+		}
+	}
+}
+
+func TestUnsetRightmostOne(t *testing.T) {
+	cases := []struct {
+		x        uint64
+		expected uint64
+	}{
+		{0, 0},
+		{1, 0},
+		{2, 0},
+		{3, 2},
+		{4, 0},
+		{5, 4},
+		{6, 4},
+		{7, 6},
+		{8, 0},
+		{9, 8},
+		{10, 8},
+		{11, 10},
+		{12, 8},
+		{13, 12},
+		{14, 12},
+		{15, 14},
+		{16, 0},
+	}
+	for _, c := range cases {
+		got := UnsetRightmostOne(c.x)
+		if got != c.expected {
+			t.Errorf(
+				"UnsetRightmostOne(0x%016x) = 0x%016x, expected 0x%016x",
+				c.x, got, c.expected,
+			)
 		}
 	}
 }
 
 func TestNextValueWithSameBitCount(t *testing.T) {
 	cases := []struct {
-		bs       BitString64
-		expected BitString64
+		x        uint64
+		expected uint64
 	}{
 		{1, 2},
 		{2, 4},
@@ -222,18 +183,102 @@ func TestNextValueWithSameBitCount(t *testing.T) {
 		{16, 32}, // 10
 	}
 	for _, c := range cases {
-		got := c.bs.NextValueWithSameBitCount()
+		got := NextValueWithSameBitCount(c.x)
 		if got != c.expected {
-			t.Errorf("BitString(0x%016x).TestNextValueWithSameBitCount() = 0x%016x, expected 0x%016x", c.bs, got, c.expected)
+			t.Errorf(
+				"TestNextValueWithSameBitCount(0x%016x) = 0x%016x, expected 0x%016x",
+				c.x, got, c.expected,
+			)
+		}
+	}
+}
+
+func TestNumberOfTrailingZerosForPowerOfTwo(t *testing.T) {
+	cases := []struct {
+		set      uint64
+		expected uint
+	}{
+		{0x0000000000000001, 0},
+		{0x0000000000000002, 1},
+		{0x0000000000000004, 2},
+		{0x0000000000000008, 3},
+		{0x0000000000000010, 4},
+		{0x0000000000000020, 5},
+		{0x0000000000000040, 6},
+		{0x0000000000000080, 7},
+		{0x0000000000000100, 8},
+		{0x0000000000000200, 9},
+		{0x0000000000000400, 10},
+		{0x0000000000000800, 11},
+		{0x0000000000001000, 12},
+		{0x0000000000002000, 13},
+		{0x0000000000004000, 14},
+		{0x0000000000008000, 15},
+		{0x0000000000010000, 16},
+		{0x0000000000020000, 17},
+		{0x0000000000040000, 18},
+		{0x0000000000080000, 19},
+		{0x0000000000100000, 20},
+		{0x0000000000200000, 21},
+		{0x0000000000400000, 22},
+		{0x0000000000800000, 23},
+		{0x0000000001000000, 24},
+		{0x0000000002000000, 25},
+		{0x0000000004000000, 26},
+		{0x0000000008000000, 27},
+		{0x0000000010000000, 28},
+		{0x0000000020000000, 29},
+		{0x0000000040000000, 30},
+		{0x0000000080000000, 31},
+		{0x0000000100000000, 32},
+		{0x0000000200000000, 33},
+		{0x0000000400000000, 34},
+		{0x0000000800000000, 35},
+		{0x0000001000000000, 36},
+		{0x0000002000000000, 37},
+		{0x0000004000000000, 38},
+		{0x0000008000000000, 39},
+		{0x0000010000000000, 40},
+		{0x0000020000000000, 41},
+		{0x0000040000000000, 42},
+		{0x0000080000000000, 43},
+		{0x0000100000000000, 44},
+		{0x0000200000000000, 45},
+		{0x0000400000000000, 46},
+		{0x0000800000000000, 47},
+		{0x0001000000000000, 48},
+		{0x0002000000000000, 49},
+		{0x0004000000000000, 50},
+		{0x0008000000000000, 51},
+		{0x0010000000000000, 52},
+		{0x0020000000000000, 53},
+		{0x0040000000000000, 54},
+		{0x0080000000000000, 55},
+		{0x0100000000000000, 56},
+		{0x0200000000000000, 57},
+		{0x0400000000000000, 58},
+		{0x0800000000000000, 59},
+		{0x1000000000000000, 60},
+		{0x2000000000000000, 61},
+		{0x4000000000000000, 62},
+		{0x8000000000000000, 63},
+	}
+	for _, c := range cases {
+		got := NumberOfTrailingZerosForPowerOfTwo(c.set)
+		if got != c.expected {
+			t.Errorf(
+				"SetToCity(0x%016x) = %d, expected %d",
+				c.set, got, c.expected,
+			)
 		}
 	}
 }
 
 func TestInsertZero(t *testing.T) {
 	cases := []struct {
-		bs       BitString64
+		x        uint64
 		index    uint
-		expected BitString64
+		expected uint64
 	}{
 		{0, 0, 0},
 		{0, 1, 0},
@@ -251,20 +296,56 @@ func TestInsertZero(t *testing.T) {
 		{3, 1, 5},
 		{3, 2, 3},
 		{3, 3, 3},
-		{177, 0, 354},
-		{177, 1, 353},
-		{177, 2, 353},
-		{177, 3, 353},
-		{177, 4, 353},
-		{177, 5, 337},
-		{177, 6, 305},
-		{177, 7, 305},
-		{177, 8, 177},
+		{177, 0, 354}, // 10110001 -> 101100010
+		{177, 1, 353}, // 10110001 -> 101100001
+		{177, 2, 353}, // 10110001 -> 101100001
+		{177, 3, 353}, // 10110001 -> 101100001
+		{177, 4, 353}, // 10110001 -> 101100001
+		{177, 5, 337}, // 10110001 -> 101010001
+		{177, 6, 305}, // 10110001 -> 100110001
+		{177, 7, 305}, // 10110001 -> 100110001
+		{177, 8, 177}, // 10110001 -> 010110001
 	}
 	for _, c := range cases {
-		got := c.bs.InsertZero(c.index)
+		got := InsertZero(c.x, c.index)
 		if got != c.expected {
-			t.Errorf("BitString(0x%016x).TestInsertZero(%d) = 0x%016x, expected 0x%016x", c.bs, c.index, got, c.expected)
+			t.Errorf(
+				"TestInsertZero(0x%016x, %d) = 0x%016x, expected 0x%016x",
+				c.x, c.index, got, c.expected,
+			)
 		}
 	}
+}
+
+func TestHello(t *testing.T) {
+	cityCount := uint(4)
+	for selectedCount := uint(1); selectedCount < cityCount; selectedCount++ {
+		for city := uint(0); city < cityCount; city++ {
+			for x := uint64(1)<<selectedCount - 1; x < (1 << (cityCount - 1)); x = NextValueWithSameBitCount(x) {
+				cities := InsertZero(x, city)
+				t.Logf("selectedCount: %d, city: %d, cities: %08s\n", selectedCount, city, ToString(cities))
+				// Find Minimum:
+				left := cities
+				right := uint64(0)
+				for left != 0 {
+					left = UnsetRightmostOne(left)
+					fromCities := left | right
+					viaCity := cities ^ fromCities
+					right = right | viaCity
+					//t.Logf("    %08s, %08s, %d, %08s\n", ToString(left), ToString(fromCities), NumberOfTrailingZerosForPowerOfTwo(viaCity), ToString(right))
+					t.Logf("    From Cities: 0x%016x, Via City: %d\n", fromCities, NumberOfTrailingZerosForPowerOfTwo(viaCity))
+				}
+			}
+			t.Logf("\n")
+		}
+		t.Logf("\n")
+	}
+}
+
+func Test25(t *testing.T) {
+	sum := 0
+	for i := 0; i < 1<<25; i++ {
+		sum += i
+	}
+	t.Logf("%d\n", sum)
 }
