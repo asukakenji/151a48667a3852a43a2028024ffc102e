89f8548d6a33ba30413e9a18751a8452da51564c
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2017-08-15 00:00:47 +0800

    Backup

--- cmd/webtier2/heldkarp.go
+++ cmd/webtier2/heldkarp.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/asukakenji/151a48667a3852a43a2028024ffc102e/bitstring"
+)
+
 /*
 
 Held-Karp
@@ -79,16 +85,61 @@ c(D, {C,E}) = min(mCD + c(C, {E}), mED + c(E, {C})) = min( 2 + 18, 16 + 34) = mi
 c(B, {D,E}) = min(mDB + c(D, {E}), mEB + c(E, {D})) = min( 6 + 27,  4 + 13) = min(33, 17) = 17 [c(E, {D})]
 c(C, {D,E}) = min(mDC + c(D, {E}), mEC + c(E, {D})) = min(18 + 27,  7 + 13) = min(45, 20) = 20 [c(E, {D})]
 
-c(B, {C,D,E}) = min(mCB + c(C, {D,E}), mDB + c(D, {C,E}), mEB + c(E, {C,D})) = min( 5 + 20,  6 + 20,  4 + 32) = min(25, 26, 36) = 25 [c(C, {D,E})]
-c(C, {B,D,E}) = min(mBC + c(B, {D,E}), mDC + c(D, {B,E}), mEC + c(E, {B,D})) = min(16 + 17, 18 + 19,  7 + 18) = min(35, 37, 25) = 25 [c(E, {B,D})]
-c(D, {B,C,E}) = min(mBD + c(B, {C,E}), mCD + c(C, {B,E}), mED + c(E, {B,C})) = min( 4 + 23,  2 + 29, 16 + 37) = min(27, 31, 53) = 27 [c(B, {C,E})]
-c(E, {B,C,D}) = min(mBE + c(B, {C,D}), mCE + c(C, {B,D}), mDE + c(D, {B,C})) = min( 2 + 33,  4 + 32,  3 + 38) = min(35, 36, 41) = 35 [c(B, {C,D})]
+c(E, {B,C,D}) = min(mDE + c(D, {B,C}), mCE + c(C, {B,D}), mBE + c(B, {C,D})) = min( 3 + 38,  4 + 32,  2 + 33) = min(41, 36, 35) = 35 [c(B, {C,D})]
+c(D, {B,C,E}) = min(mED + c(E, {B,C}), mCD + c(C, {B,E}), mBD + c(B, {C,E})) = min(16 + 37,  2 + 29,  4 + 23) = min(53, 31, 27) = 27 [c(B, {C,E})]
+c(C, {B,D,E}) = min(mEC + c(E, {B,D}), mDC + c(D, {B,E}), mBC + c(B, {D,E})) = min( 7 + 18, 18 + 19, 16 + 17) = min(25, 37, 35) = 25 [c(E, {B,D})]
+c(B, {C,D,E}) = min(mEB + c(E, {C,D}), mDB + c(D, {C,E}), mCB + c(C, {D,E})) = min( 4 + 32,  6 + 20,  5 + 20) = min(36, 26, 25) = 25 [c(C, {D,E})]
 
-c(A, {B,C,D,E}) = min(mBA + c(B, {C,D,E}), mCA + c(C, {B,D,E}), mDA + c(D, {B,C,E}), mEA + c(E, {B,C,D})
-                = min(15 + 25, 3 + 25, 19 + 27, 16 + 35)
-                = min(40, 28, 46, 51)
+c(A, {B,C,D,E}) = min(mEA + c(E, {B,C,D}), mDA + c(D, {B,C,E}), mCA + c(C, {B,D,E}), mBA + c(B, {C,D,E}))
+                = min(16 + 35, 19 + 27, 3 + 25, 15 + 25)
+                = min(51, 46, 28, 40)
                 = 28 [c(C, {B,D,E})]
 
 Tour: A <- C <- E <- B <- D <- A
 
+       +----+                 +----+                 +----+                 +----+
+       | 20 |                 | 30 |                 | 10 |                 | 11 |
+       +----+                 +----+                 +----+                 +----+
+
++----+ +----+ +----+   +----+ +----+ +----+   +----+ +----+ +----+   +----+ +----+ +----+
+| 36 | | 24 | | 22 |   | 35 | | 32 | | 34 |   | 16 | | 28 | | 13 |   | 15 | | 18 | | 27 |
++----+ +----+ +----+   +----+ +----+ +----+   +----+ +----+ +----+   +----+ +----+ +----+
+
+    +----+----+   +----+----+   +----+----+   +----+----+   +----+----+   +----+----+
+    | 38 | 37 |   | 32 | 18 |   | 29 | 19 |   | 33 | 32 |   | 23 | 20 |   | 17 | 20 |
+    +----+----+   +----+----+   +----+----+   +----+----+   +----+----+   +----+----+
+
+       +----+                 +----+                 +----+                 +----+
+       | 35 |                 | 27 |                 | 25 |                 | 25 |
+       +----+                 +----+                 +----+                 +----+
+
+                                          +----+
+                                          | 28 |
+                                          +----+
 */
+
+func Hello() {
+	cityCount := uint(4)
+	onesLimit := uint64(1) << (cityCount - 1)
+	for selectedCount := uint(1); selectedCount < cityCount; selectedCount++ {
+		for city := uint(0); city < cityCount; city++ {
+			for ones := bitstring.Ones(selectedCount); ones < onesLimit; ones = bitstring.NextNumberWithSameBitCount(ones) {
+				cities := bitstring.InsertZero(ones, city)
+				fmt.Printf("selectedCount: %d, city: %d, cities: %08s\n", selectedCount, city, bitstring.ToString(cities))
+				// Find Minimum:
+				left := cities
+				right := uint64(0)
+				for left != 0 {
+					left = bitstring.ResetRightmostOne(left)
+					fromCities := left | right
+					viaCity := cities ^ fromCities
+					right = right | viaCity
+					//fmt.Printf("    %08s, %08s, %d, %08s\n", ToString(left), ToString(fromCities), NumberOfTrailingZerosForPowerOfTwo(viaCity), ToString(right))
+					fmt.Printf("    From Cities: 0x%016x, Via City: %d\n", fromCities, bitstring.NumberOfTrailingZerosForPowerOfTwo(viaCity))
+				}
+			}
+			fmt.Printf("\n")
+		}
+		fmt.Printf("\n")
+	}
+}
