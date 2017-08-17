dc68a001da472801cdaf5b472e4eb33a976d77ef
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2017-08-17 20:04:17 +0800

    Added documentations

--- constant/constant.go
+++ constant/constant.go
@@ -1,7 +1,12 @@
 package constant
 
 const (
-	MaxUint  = ^uint(0)
-	MaxInt   = int(MaxUint >> 1)
+	// MaxUint is the maximum value that could be represented with a uint type.
+	MaxUint = ^uint(0)
+
+	// MaxInt is the maximum value that could be represented with an int type.
+	MaxInt = int(MaxUint >> 1)
+
+	// Infinity is the value that represents an unreachable distance.
 	Infinity = -1
 )
