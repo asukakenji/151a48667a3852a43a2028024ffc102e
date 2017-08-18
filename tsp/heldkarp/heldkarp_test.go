e27830945981e257e6e6bc258fe4630d7cc99ed3
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2017-08-18 12:48:23 +0800

    Replaced -1 by constant.Infinity; Added a real test case

--- tsp/heldkarp/heldkarp_test.go
+++ tsp/heldkarp/heldkarp_test.go
@@ -4,6 +4,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/asukakenji/151a48667a3852a43a2028024ffc102e/constant"
 	"github.com/asukakenji/151a48667a3852a43a2028024ffc102e/matrix"
 )
 
@@ -19,45 +20,51 @@ func DeepEqualOneOf(gotPath []int, expectedPaths [][]int) bool {
 var matrices = [][][]int{
 	// https://youtu.be/-JjA4BLQyqE
 	{
-		{-1, 1, 15, 6},
-		{2, -1, 7, 3},
-		{9, 6, -1, 12},
-		{10, 4, 8, -1},
+		{constant.Infinity, 1, 15, 6},
+		{2, constant.Infinity, 7, 3},
+		{9, 6, constant.Infinity, 12},
+		{10, 4, 8, constant.Infinity},
 	},
 	// https://youtu.be/aQB_Y9D5pdw
 	{
-		{-1, 10, 15, 20},
-		{5, -1, 9, 10},
-		{6, 13, -1, 12},
-		{8, 8, 9, -1},
+		{constant.Infinity, 10, 15, 20},
+		{5, constant.Infinity, 9, 10},
+		{6, 13, constant.Infinity, 12},
+		{8, 8, 9, constant.Infinity},
 	},
 	// https://youtu.be/vNqE_LDTsa0
 	{
-		{-1, 7, 6, 8, 4},
-		{7, -1, 8, 5, 6},
-		{6, 8, -1, 9, 7},
-		{8, 5, 9, -1, 8},
-		{4, 6, 7, 8, -1},
+		{constant.Infinity, 7, 6, 8, 4},
+		{7, constant.Infinity, 8, 5, 6},
+		{6, 8, constant.Infinity, 9, 7},
+		{8, 5, 9, constant.Infinity, 8},
+		{4, 6, 7, 8, constant.Infinity},
 	},
 	// https://youtu.be/FJkT_dRjX94
 	// https://youtu.be/KzWC-t1y8Ac
 	{
-		{-1, 20, 30, 10, 11},
-		{15, -1, 16, 4, 2},
-		{3, 5, -1, 2, 4},
-		{19, 6, 18, -1, 3},
-		{16, 4, 7, 16, -1},
+		{constant.Infinity, 20, 30, 10, 11},
+		{15, constant.Infinity, 16, 4, 2},
+		{3, 5, constant.Infinity, 2, 4},
+		{19, 6, 18, constant.Infinity, 3},
+		{16, 4, 7, 16, constant.Infinity},
 	},
 	// https://youtu.be/-cLsEHP0qt0
 	// https://youtu.be/nN4K8xA8ShM
 	// https://youtu.be/LjvdXKsvUpU
 	// https://youtu.be/hjZDDz3r1es
 	{
-		{-1, 10, 8, 9, 7},
-		{10, -1, 10, 5, 6},
-		{8, 10, -1, 8, 9},
-		{9, 5, 8, -1, 6},
-		{7, 6, 9, 6, -1},
+		{constant.Infinity, 10, 8, 9, 7},
+		{10, constant.Infinity, 10, 5, 6},
+		{8, 10, constant.Infinity, 8, 9},
+		{9, 5, 8, constant.Infinity, 6},
+		{7, 6, 9, 6, constant.Infinity},
+	},
+	// Example
+	{
+		{constant.Infinity, 15518, 9667},
+		{15223, constant.Infinity, 8333},
+		{10329, 8464, constant.Infinity},
 	},
 }
 
@@ -92,6 +99,11 @@ func TestTravellingSalesmanTour(t *testing.T) {
 			matrices[4], 34, [][]int{
 				{0, 2, 3, 1, 4}, // A -> C -> D -> B -> E -> A
 				{0, 4, 1, 3, 2}, // A -> E -> B -> D -> C -> A (symmetric)
+			},
+		},
+		{
+			matrices[5], 33354, [][]int{
+				{0, 2, 1},
 			},
 		},
 	}
@@ -138,6 +150,11 @@ func TestTravellingSalesmanPath(t *testing.T) {
 			matrices[4], 26, [][]int{
 				//{0, 2, 3, 1, 4}, // A -> C -> D -> B -> E (cost = 27, symmetric in tour)
 				{0, 4, 1, 3, 2}, // A -> E -> B -> D -> C
+			},
+		},
+		{
+			matrices[5], 18131, [][]int{
+				{0, 2, 1},
 			},
 		},
 	}
